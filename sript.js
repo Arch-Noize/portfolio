4bb438a78df2d1d2706c1f55923e5846a1b9b55c
Arch-Noize <santiagonoize@gmail.com>
2023-06-19 13:54:09 -0400

    Add menu style

--- sript.js
+++ sript.js
@@ -1,17 +1,18 @@
 let burger = document.querySelector('.burger');
 
+let cancel = document.querySelector('.cancel');
+
+let items = document.querySelector('.menu-items');
+
 function menu() {
     let menu = document.querySelector('.menu');
 
-    // make sure style mobile menu with css
-
-
-    // absolute position to hide
-
-
-    // js to hide and unhide
-
     menu.classList.toggle('menu-display');
+
 }
 
-burger.addEventListener('click', menu)
+burger.addEventListener('click', menu);
+
+cancel.addEventListener('click', menu);
+
+items.addEventListener('click', menu);
