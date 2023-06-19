ab78ef0e0ba9182b706ffe39d87db051a3f72049
Arch-Noize <santiagonoize@gmail.com>
2023-06-19 10:31:17 -0400

    Add button javascript function

---
+++ sript.js
@@ -0,0 +1,17 @@
+let burger = document.querySelector('.burger');
+
+function menu() {
+    let menu = document.querySelector('.menu');
+
+    // make sure style mobile menu with css
+
+
+    // absolute position to hide
+
+
+    // js to hide and unhide
+
+    menu.classList.toggle('menu-display');
+}
+
+burger.addEventListener('click', menu)
