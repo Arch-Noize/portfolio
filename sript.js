a7fbc17bdf81a68f1430ba6aa64a31b39502d0e6
Arch-Noize <santiagonoize@gmail.com>
2023-06-20 11:58:34 -0400

    Add modal window

--- sript.js
+++ sript.js
@@ -4,16 +4,38 @@ const cancel = document.querySelector('.cancel');
 
 const items = document.querySelectorAll('.menu-item');
 
+const modalBtn = document.querySelectorAll('.modal-btn');
+
+const closeBtn = document.querySelector('.close-btn');
+
 function menu() {
   const menu = document.querySelector('.menu');
 
   menu.classList.toggle('menu-display');
 }
 
+function project(){
+  const modal = document.querySelector('.modal-container');
+
+  modal.classList.toggle('open-overlay');
+}
+
+function projectClose(){
+  modal.classList.remove('open-overlay');
+}
+
 burger.addEventListener('click', menu);
 
 cancel.addEventListener('click', menu);
 
+modalBtn.addEventListener('click', project);
+
+closeBtn.addEventListener('click', project);
+
 items.forEach((menuItem) => {
   menuItem.addEventListener("click", menu);
+});
+
+modalBtn.forEach((btn) => {
+  btn.addEventListener('click', project);
 });
