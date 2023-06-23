735d47eee97dda1386f33b96114d7b69a82975d9
Arch-Noize <santiagonoize@gmail.com>
2023-06-23 10:36:43 -0400

    fix linter error

--- sript.js
+++ sript.js
@@ -1,23 +1,23 @@
-// Burger / Navbar 
+// Burger / Navbar
 
-const burger = document.querySelector(".burger");
+const burger = document.querySelector('.burger');
 
-const cancel = document.querySelector(".cancel");
+const cancel = document.querySelector('.cancel');
 
-const items = document.querySelectorAll(".menu-item");
+const items = document.querySelectorAll('.menu-item');
 
 function menu() {
-  const menu = document.querySelector(".menu");
+  const menu = document.querySelector('.menu');
 
-  menu.classList.toggle("menu-display");
+  menu.classList.toggle('menu-display');
 }
 
-burger.addEventListener("click", menu);
+burger.addEventListener('click', menu);
 
-cancel.addEventListener("click", menu);
+cancel.addEventListener('click', menu);
 
 items.forEach((menuItem) => {
-  menuItem.addEventListener("click", menu);
+  menuItem.addEventListener('click', menu);
 });
 
 // Works
@@ -25,47 +25,46 @@ items.forEach((menuItem) => {
 const story = [
   {
     id: 1,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
-  }, 
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+  },
   {
     id: 2,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
   },
   {
     id: 3,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
-  }, 
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+  },
   {
     id: 4,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
-  }, 
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+  },
   {
     id: 5,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
-  }, 
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+  },
   {
     id: 6,
-    name: "Multi-Post Stories Gain+Glory",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    button: "See Project",
-  }
+    name: 'Multi-Post Stories Gain+Glory',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    button: 'See Project',
+  },
 ];
 
-const recent = document.querySelector(".works");
+const recent = document.querySelector('.works');
 
-function stories(){
-  let displayStory = story.map(function(item){
-    return `<div class="recent">
+function stories() {
+  let displayStory = story.map((item) => `<div class="recent">
               <div class="story">
                 <div class="grid">
                   <div class="grid-item">
@@ -86,35 +85,32 @@ function stories(){
                   </div>
                 </div>
               </div>
-            </div>`;
-  });
-  displayStory = displayStory.join("");
+            </div>`);
+  displayStory = displayStory.join('');
   recent.innerHTML = displayStory;
 }
 
-window.addEventListener("DOMContentLoaded", stories);
+window.addEventListener('DOMContentLoaded', stories);
 
 // Modal pop-up window
 
-const projPop = document.querySelector(".project-pop")
+const projPop = document.querySelector('.project-pop');
 
 const modalMenu = [
   {
     id: 1,
-    name: "Keeping track of hundreds of components",
+    name: 'Keeping track of hundreds of components',
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.",
-    image: "./Images/project.png",
-    tech: ["Ruby on Rails","CSS","JavaScript","HTML"],
-    live: "https://arch-noize.github.io/portfolio-microverse/",
-    source: "https://github.com/Arch-Noize/portfolio-microverse",
-    imgCancel: "./Images/cancel.png",
-  }
+    image: './Images/project.png',
+    tech: ['Ruby on Rails', 'CSS', 'JavaScript', 'HTML'],
+    live: 'https://arch-noize.github.io/portfolio-microverse/',
+    source: 'https://github.com/Arch-Noize/portfolio-microverse',
+    imgCancel: './Images/cancel.png',
+  },
 ];
 
-function popup(){
-  let displayWork = modalMenu.map(function(item){
-  
-    return `<div class="modal-container">
+function popup() {
+  let displayWork = modalMenu.map((item) => `<div class="modal-container">
               <div class="modal-item">
                 <figure class="close-btn">
                   <img src="${item.imgCancel}" alt="">
@@ -153,28 +149,27 @@ function popup(){
                       <a href="${item.source}"> See Source </a>
                   </button>
               </div>
-          </div>`;
-  });
-  displayWork = displayWork.join("");
+          </div>`);
+  displayWork = displayWork.join('');
   projPop.innerHTML = displayWork;
 }
 
-function projectDisplay(){
-  const modal = document.querySelector(".project-pop");
+function projectDisplay() {
+  const modal = document.querySelector('.project-pop');
 
-  modal.classList.toggle("open-pop");
+  modal.classList.toggle('open-pop');
 }
 
-window.addEventListener("DOMContentLoaded", popup);
+window.addEventListener('DOMContentLoaded', popup);
 
-window.addEventListener("DOMContentLoaded", function(){
-  let btn = document.querySelectorAll(".modal-btn");
+window.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelectorAll('.modal-btn');
 
   btn.forEach((b) => {
     b.addEventListener('click', projectDisplay);
   });
 
-  let cancel = document.querySelector(".close-btn");
+  const cancel = document.querySelector('.close-btn');
 
   cancel.addEventListener('click', projectDisplay);
 });
@@ -187,18 +182,17 @@ const email = document.getElementById('mail');
 
 const error = document.getElementById('error');
 
-email.addEventListener("click", function () {
-  if (email === document.activeElement) error.style.display = "none";
+email.addEventListener('click', () => {
+  if (email === document.activeElement) error.style.display = 'none';
 });
 
-form.addEventListener("submit", (event) => {
+form.addEventListener('submit', (event) => {
   const regexMail = /[A-Z]/;
   if (regexMail.test(email.value)) {
     event.preventDefault();
     email.classList.add('error-shadow');
-    error.style.display = "block";
+    error.style.display = 'block';
   }
-
 });
 
 // Keep Data
